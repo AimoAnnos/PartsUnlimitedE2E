b04e0b4cd19b5e156ec635f2ce1afd60671ee9bb
Mie Ihte <joojoo@email.com>
2022-02-03 12:39:19 +0200

    added commits

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // jeejee
+        // jeejee jee
         [Key]
         public int CartItemId { get; set; }
 
